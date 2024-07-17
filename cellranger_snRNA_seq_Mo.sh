2efc9e105d884e67eb57f0b592e30df440d88c93
nnkarma12 <nnkarma12@gmail.com>
2024-07-17 15:13:56 +0200

    Add files via upload

---
+++ cellranger_snRNA_seq_Mo.sh
@@ -0,0 +1,25 @@
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a001_02 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a001_02/ --sample=20084a001_02 --create-bam=true > 20084a001_02.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a002_02 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a002_02/ --sample=20084a002_02 --create-bam=true > 20084a002_02.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a003_02 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a003_02/ --sample=20084a003_02 --create-bam=true > 20084a003_02.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a004_02 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a004_02/ --sample=20084a004_02 --create-bam=true > 20084a004_02.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a005_01 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a005_01/ --sample=20084a005_01 --create-bam=true > 20084a005_01.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a006_01 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a006_01/ --sample=20084a006_01 --create-bam=true > 20084a006_01.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a007_01 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a007_01/ --sample=20084a007_01 --create-bam=true > 20084a007_01.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a008_01 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a008_01/ --sample=20084a008_01 --create-bam=true > 20084a008_01.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a009_04 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a009_04/ --sample=20084a009_04 --create-bam=true > 20084a009_04.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a010_04 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a010_04/ --sample=20084a010_04 --create-bam=true > 20084a010_04.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a011_03 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a011_03/ --sample=20084a011_03 --create-bam=true > 20084a011_03.err
+
+cd /mnt/md0/Nasser/20084I_0552_Mo_snRNA && /mnt/md0/MultiOmics/Tools/Nasser/cellranger-8.0.0/cellranger count --id=20084a012_01 --transcriptome=/mnt/md0/MultiOmics/Database/Nasser/refdata-gex-GRCh38-2024-A --fastqs=/mnt/md0/Mohammad/20084I_0552_Mohammad_nuclei/Sample_20084a012_01/ --sample=20084a012_01 --create-bam=true > 20084a012_01.err
+
+
